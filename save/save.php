37fc6a795bede2f83195b156d5bc4092271c76f9
wakufactory <waku@lang.co.jp>
2021-01-06 16:24:51 +0900

    update load json

--- save/save.php
+++ save/save.php
@@ -1,4 +1,8 @@
 <?php
+if(!preg_match("/^[a-zA-Z0-9\-\_]+$/",$_POST['fname'])) {
+	echo '{"status":-1}' ;
+	exit ;
+}
 $f = $_POST['fname'].".json" ;
 $d = $_POST['data'] ;
 $fp = fopen("data/".$f,"w") ;
